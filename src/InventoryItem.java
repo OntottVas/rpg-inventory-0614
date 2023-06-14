6f99b442c4254046a812d5688dc6e934a561cca5
Geree <smithzero94@gmail.com>
2023-06-14 19:42:53 +0200

    InventoryItem class defined by README.md file.

--- src/InventoryItem.java
+++ src/InventoryItem.java
@@ -3,12 +3,37 @@ public class InventoryItem {
         // egy neve (name),
         // egy mennyisége (quantity)
         // és egy ára (price)
+    private String name;
+    private int quantity;
+    private double price;
 
     // Írj egy konstruktort, ami beállítja az InventoryItem minden értékét!
+    public InventoryItem(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
 
     // Írj gettereket az összes fieldhez!
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 
     // Írj settert a mennyiséghez!
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
     // Írj egy calculateValue() metódust, ami kiszámolja a termék össz. értékét (a mennyiség és egységár függvényében)
+    public double calculateValue(int quantity, double price) {
+        return quantity * price;
+    }
 }
